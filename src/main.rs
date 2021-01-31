2168b8a84247a3fe31ca2c7a79f36d44e5f4a9e2
Tom <thomas.spencer@codurance.com>
2021-01-31 14:26:40 +0000

    frst_word function

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,45 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("This is s1 and len: {}, {}", s1, len);
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // r1 and r2 are no longer used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+
+    let reference_to_nothing = dangle();
+    println!("{}", reference_to_nothing);
+    let our_string = &mut String::from("hello123");
+    let result = first_word(&our_string);
+
+    println!("this is the result: {}", result);
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn dangle() -> String {
+    let s = String::from("hello");
+
+    s
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
 
 fn calculate_length(s: &String) -> usize { // s is a reference to a String
